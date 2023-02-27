5e59eae0c655e37f50bda73f9ec3bd479e6f6086
Sander Blijham <sander.blijham@gmail.com>
2023-02-27 21:36:56 +0100

    Changed routing to HomePage.vue

--- vuetify-project/src/views/Home.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <HomeCards />
-</template>
-
-<script setup>
-  import HomeCards from '@/components/HomeCards.vue'
-</script>
---
+++ vuetify-project/src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <default-bar />
+    <default-cards />
+    <default-bottom-bar />
+  </v-app>
+</template>
+
+<script setup>
+  import DefaultBar from '@/layouts\\default/AppBar.vue'
+  import DefaultCards from '@/components/HomeCards.vue'
+  import DefaultBottomBar from '@/layouts\\default/BottomBar.vue'
+</script>
